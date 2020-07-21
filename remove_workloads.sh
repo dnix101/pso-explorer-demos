b1da3fab343fe44214377d6b5fed167617dd2af2
Remko Deenik <remko@continuance.nl>
2020-07-21 10:21:19 +0200

    Update remove_workloads.sh

--- remove_workloads.sh
+++ remove_workloads.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-kubectl delete -f snapshot-alpha/snapshot.yaml -n wordpress-1
-kubectl delete -f snapshot-alpha/snapshot.yaml -n wordpress-2
-kubectl delete -f snapshot-alpha/snapshot.yaml -n wordpress-3
-kubectl delete -f snapshot-alpha/snapshot.yaml -n wordpress-4
 
-kubectl delete -f snapshot-alpha/snapshot-fio.yaml -n fio-1
-kubectl delete -f snapshot-alpha/snapshot-fio.yaml -n fio-2
+# Remove snapshots for Wordpress
+kubectl delete volumesnapshot --all -n wordpress-1
+kubectl delete volumesnapshot --all -n wordpress-2
+kubectl delete volumesnapshot --all -n wordpress-3
+kubectl delete volumesnapshot --all -n wordpress-4
 
+# Remove snapshots for workbench
+kubectl delete volumesnapshot --all -n fio-1
+kubectl delete volumesnapshot --all -n fio-2
+
+# Uninstall Wordpress applications
 helm uninstall wordpress -n wordpress-1
 kubectl delete pvc --all -n wordpress-1
 kubectl delete namespace wordpress-1
@@ -23,13 +27,18 @@ helm uninstall wordpress -n wordpress-4
 kubectl delete pvc --all -n wordpress-4
 kubectl delete namespace wordpress-4
 
+# Remove workbench jobs and volumes
 kubectl delete -f fio/fio-block.yaml -n fio-1
 kubectl delete namespace fio-1
 
 kubectl delete -f fio/fio-block.yaml -n fio-2
 kubectl delete namespace fio-2
 
-kubectl delete -f snapshot-alpha/snapshot-elastic.yaml -n elastic
+kubectl delete -f fio/fio-file.yaml -n fio-3
+kubectl delete namespace fio-3
+
+# Remove Elastic Search
+kubectl delete volumesnapshot --all -n elastic
 kubectl patch pvc elasticsearch-data-quickstart-es-default-0 -p '{"metadata":{"finalizers":null}}' -n elastic
 kubectl delete -f elastic/elastic.yaml -n elastic
 kubectl delete -f elastic/all-in-one.yaml
