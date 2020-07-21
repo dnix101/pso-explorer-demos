648a623a8c0a5329ea7c5b013e0cf1aaeb4f940a
Remko Deenik <remko@continuance.nl>
2020-07-21 11:37:21 +0200

    Update remove_pso.sh

--- remove_pso.sh
+++ remove_pso.sh
@@ -2,5 +2,5 @@
 
 helm uninstall pure-storage-driver -n pure-csi
 kubectl delete namespace pure-csi
-helm uninstall pso-analytics -n pso-analytics
-kubectl delete namespace pso-analytics
+helm uninstall pso-explorer -n psox
+kubectl delete namespace psox
